9039473bebe3f23e845b485df2c0121c69f38ba9
Daniel Morales <mralejo99@gmail.com>
2021-11-23 23:07:33 -0500

    added constructor example in java

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -3,5 +3,14 @@ package JAVA;
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        Account driver = new Account("Daniel Morales", "1144108017");
+        Car car = new Car("AMQ123", driver);
+        car.passenger = 4;
+        car.printDataCar();
+
+        Account driver2 = new Account("Adriana Sevillano", "66903734");
+        Car car2 = new Car("QWE567", driver);
+        car2.passenger = 3;
+        car2.printDataCar();
     }
 }
--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,2 +1,11 @@
-if __name__=='__main__':
-    print("Hello world")
+from car import Car
+if __name__ == '__main__':
+    car = Car()
+    car.license = "AMS123"
+    car.driver = "Daniel Morales"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "KPX993"
+    car2.driver = "Adriana"
+    print(vars(car2))
