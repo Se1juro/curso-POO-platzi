ceac06e23b4ab71a8e73bbb441e30e712b8b7bd8
Daniel Morales <mralejo99@gmail.com>
2021-11-24 21:53:28 -0500

    añadidos mas ejemplos de herencia, python y javascript

--- JS/Version ES6/index.js
+++ JS/Version ES6/index.js
@@ -1,7 +1,16 @@
 const { Account } = require("./Account");
 const { Car } = require("./Car");
+const { UberX } = require("./uberX");
 
 const driver = new Account("Daniel Morales", "1144108017");
 const car = new Car("KPX993", driver);
 car.passenger = 4;
 car.printDataCar();
+
+const uberX = new UberX(
+  "KPX993",
+  new Account("Daniel", "1144108017"),
+  "Renault",
+  "Kwid"
+);
+uberX.printDataCar();
