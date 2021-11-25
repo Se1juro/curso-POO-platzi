d3a26bab527c24ea2f968e2932457920acb88b9d
Daniel Morales <mralejo99@gmail.com>
2021-11-24 22:24:28 -0500

    se agregan ejemplos de herencia en PYTHON, PHP y JS

--- PHP/main.php
+++ PHP/main.php
@@ -1,9 +1,14 @@
 <?php
 require_once('./car.php');
+require_once('./uberX.php');
+require_once('./uberPool.php');
 require_once('./account.php');
 
 $driver = new Account("Daniel Morales", "114410807");
-$car = new Car("KPX993", $driver);
+$uberX = new UberX("KPX993", new Account("Daniel", "1144108017"), "Renault", "Kwid");
 
 
-echo "<h3>" . $car->printDataCar() . "</h3>";
+echo "<h3>" . $uberX->printDataCar() . "</h3>";
+
+$uberPool = new UberPool("CPC835", new Account("Jhon", "1111111"), "Hyundai", "Accent");
+echo "<h3>" . $uberPool->printDataCar() . "</h3>";
--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,5 +1,6 @@
 from account import Account
 from car import Car
+from uberX import UberX
 if __name__ == '__main__':
     driver = Account("Daniel Morales", "1144108017")
     car = Car("KPX993", driver)
@@ -8,3 +9,6 @@ if __name__ == '__main__':
     driver2 = Account("JJ Morales", "94415464")
     car2 = Car("CPC835", driver2)
     print(vars(car2))
+
+    uberX = UberX("KPX992", Account("Daniel", "1234"), "Renault", "Kwid")
+    print(vars(uberX))
