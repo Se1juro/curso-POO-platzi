2f3d2a3a79a2cc210fc444fa6cce1b8d3b736ae0
Daniel Morales <mralejo99@gmail.com>
2021-11-30 20:06:57 -0500

    implementacion de abstraccion en JAVA

--- JAVA/Car.java
+++ JAVA/Car.java
@@ -1,9 +1,9 @@
 package JAVA;
 
 class Car {
-    Integer id;
-    String license;
-    Account driver;
+    private Integer id;
+    private String license;
+    private Account driver;
     private Integer passenger;
 
     public Car(String license, Account driver) {
@@ -27,4 +27,29 @@ class Car {
             System.out.println("La cantidad minima de pasajeros son 4.");
         }
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public Account getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Account driver) {
+        this.driver = driver;
+    }
+
 }
